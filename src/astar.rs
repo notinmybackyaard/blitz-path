28c6a4a1146754dd16a8850f32dab10744eb1935
notinmybackyaard <theloder@naver.com>
2022-07-11 13:12:04 +0900

    Create astar modified version

--- src/astar.rs
+++ src/astar.rs
@@ -105,3 +105,82 @@ pub fn a_star_path<U, T: Map2D<U>>(map: &T, start: Coords2D, goal: Coords2D) -> 
 
     None
 }
+
+pub fn a_star_path_modified<F>(start: Coords2D, goal: Coords2D, successors: F) -> Option<Coords2D>
+    where F: Fn(Coords2D) -> Vec<Coords2D>
+{
+    if start == goal {
+        return None;
+    }
+
+    //Initialize open and closed lists
+    let capacity = (1 + start.0 + 1 - start.0 - 1) * (1 + start.1 + 1 - start.1 - 1);
+    let mut open = BinaryHeap::with_capacity(capacity);
+    let mut closed = Vec::<Node>::with_capacity(capacity);
+
+    //Push start node to open list
+    open.push(Node {
+        f: distance(start, goal),
+        g: 0.0,
+        h: distance(start, goal),
+        position: start,
+        parent: start,
+    });
+
+    //Examine the nodes
+    while let Some(node_current) = open.pop() {
+        //If this is the target node return the distance to get there
+        if node_current.position == goal {
+            let path = rewind(&node_current, &closed);
+            return Some(path[0]);
+        }
+
+        //Setup successor nodes
+        for successor in successors(node_current.position) {
+            //Calculate distances
+            let distance_to_goal = distance(successor, goal);
+            let distance_from_parent = distance(node_current.position, successor);
+            let total_distance = node_current.g + distance_from_parent;
+
+            //Check if node is on closed list
+            if let Some(closed) = closed.iter().find(|x| x.position == successor) {
+                //and skip if existing score is better
+                if closed.f < total_distance + distance_to_goal {
+                    continue;
+                }
+            }
+
+            //See if node is on open list
+            if let Some(old_open) = open.iter().find(|x| x.position == successor) {
+                //Continue if we already have a better result
+                if old_open.g < total_distance {
+                    continue;
+                }
+            }
+
+            //If sill best distance build node and push to open list
+            //Build node from distances
+            let node_successor = Node {
+                f: total_distance + distance_to_goal,
+                g: total_distance,
+                h: distance_to_goal,
+                position: successor,
+                parent: node_current.position,
+            };
+
+            open.push(node_successor);
+        }
+
+        //Push current node to closed list and remove existing entry if duplicate
+        if let Some(index) = closed
+            .iter()
+            .position(|x| x.position == node_current.position)
+        {
+            closed.remove(index);
+        }
+        closed.push(node_current);
+    }
+
+    None
+}
+
